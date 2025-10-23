4226abf2d080121ad669363c9c8f97a12ae05082
Netanel Amar <NetanelCA2@gmail.com>
2025-10-23 19:51:45 +0300

    Refactor network layer to use Hilt DI
    Replaced the RetrofitInstance singleton with a Hilt-based NetworkModule for dependency injection. Added TmdbApp class to enable Hilt. Updated Constants with MOVIES_URL and IMAGES_URL. This improves testability and modularity of the network layer.

---
+++ app/src/main/java/com/netanel/tmdb/TmdbApp.kt
@@ -0,0 +1,14 @@
+package com.netanel.tmdb
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by netanelamar on 23/10/2025.
+ * NetanelCA2@gmail.com
+ */
+@HiltAndroidApp
+class TmdbApp: Application() {
+
+}
--- app/src/main/java/com/netanel/tmdb/domain/Constants.kt
+++ app/src/main/java/com/netanel/tmdb/domain/Constants.kt
@@ -2,6 +2,7 @@ package com.netanel.tmdb.domain
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY =
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2FkMjcxNTAwNjI5MTU3OGVlOTBjYjVhNmM2NGViMiIsInN1YiI6IjYyMjY0NWUzNzE0NThmMDAxOTdjMzI2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ngrvxEa3zSZclYbcVgOWk_u4LccQ9dC4mW08xPmLmok"
+    const val MOVIES_URL = "https://api.themoviedb.org/3/"
+    const val IMAGES_URL = "https://image.tmdb.org/t/p/original/"
+    const val API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2FkMjcxNTAwNjI5MTU3OGVlOTBjYjVhNmM2NGViMiIsInN1YiI6IjYyMjY0NWUzNzE0NThmMDAxOTdjMzI2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ngrvxEa3zSZclYbcVgOWk_u4LccQ9dC4mW08xPmLmok"
 }
