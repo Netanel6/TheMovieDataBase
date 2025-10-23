ee8b55d35bee99003c606bc1ae7dfb09b2df90f0
Netanel Amar <NetanelCA2@gmail.com>
2025-10-23 21:15:31 +0300

    Update MovieItem.kt

--- app/src/main/java/com/netanel/tmdb/ui/composables/MovieItem.kt
+++ app/src/main/java/com/netanel/tmdb/ui/composables/MovieItem.kt
@@ -1,9 +1,24 @@
 package com.netanel.tmdb.ui.composables
 
+import android.provider.Settings.Global.getString
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.netanel.tmdb.R
+import com.netanel.tmdb.domain.Constants
 import com.netanel.tmdb.domain.movie.model.Movie
 
 
@@ -12,32 +27,39 @@ import com.netanel.tmdb.domain.movie.model.Movie
  * NetanelCA2@gmail.com
  */
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun MovieItem(movie: Movie) {
-    Column { 
-        Text(text = movie.title)
-        Text(text = movie.overview)
+    Column(Modifier.wrapContentSize()) {
+        GlideImage(
+            model = Constants.IMAGES_URL.plus(movie.posterPath),
+            contentDescription = movie.originalTitle,
+            modifier = Modifier.size(440.dp),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.None
+        )
     }
-    
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MovieItemPreview() {
-    MovieItem(Movie(
-        isAdult = false,
-        backdropPath = "/7QirCB1o80NEFpQGlQRZerZbQEp.jpg", // A valid backdrop path
-        genreIds = listOf(10749, 18),
-        id = 1156594,
-        originalLanguage = "es",
-        originalTitle = "Culpa nuestra",
-        overview = "Jenna and Lion's wedding brings about the long-awaited reunion between Noah and Nick after their breakup. Nick's inability to forgive Noah stands as an insurmountable barrier. He, heir to his grandfather's businesses, and she, starting her professional life, resist fueling a flame that's still alive. But now that their paths have crossed again, will love be stronger than resentment?",
-        popularity = 1096.6654,
-        posterPath = "/yzqHt4m1SeY9FbPrfZ0C2Hi9x1s.jpg", // A valid poster path
-        releaseDate = "2025-10-15", // Date in string format
-        title = "Our Fault",
-        isVideo = false,
-        voteAverage = 7.854,
-        voteCount = 305
-    ))
+    MovieItem(
+        Movie(
+            isAdult = false,
+            backdropPath = "/7QirCB1o80NEFpQGlQRZerZbQEp.jpg", // A valid backdrop path
+            genreIds = listOf(10749, 18),
+            id = 1156594,
+            originalLanguage = "es",
+            originalTitle = "Culpa nuestra",
+            overview = "Jenna and Lion's wedding brings about the long-awaited reunion between Noah and Nick after their breakup. Nick's inability to forgive Noah stands as an insurmountable barrier. He, heir to his grandfather's businesses, and she, starting her professional life, resist fueling a flame that's still alive. But now that their paths have crossed again, will love be stronger than resentment?",
+            popularity = 1096.6654,
+            posterPath = "/yzqHt4m1SeY9FbPrfZ0C2Hi9x1s.jpg", // A valid poster path
+            releaseDate = "2025-10-15", // Date in string format
+            title = "Our Fault",
+            isVideo = false,
+            voteAverage = 7.854,
+            voteCount = 305
+        )
+    )
 }
